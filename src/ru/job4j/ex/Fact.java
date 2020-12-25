02bc4c806e6f75531dd101d6c1d6161be1b49d22
Buthead9250 <Phil9250@yandex.ru>
2020-12-25 16:06:46 +0700

    0.2. Кидаем исключение - throw new RuntimeException [#403050]

---
+++ src/ru/job4j/ex/Fact.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+import java.lang.IllegalArgumentException;
+
+public class Fact {
+    public static void main(String[] args) {
+
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n should be more then 0");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
